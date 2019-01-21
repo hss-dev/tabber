de78231e0c19b2d72996553ef059836d2342fbcf
Mick Carter <mick.carter@hssnet.com>
2019-01-21 13:36:50 +0000

    Split off element from reg

--- tabber.js
+++ tabber.js
@@ -17,7 +17,7 @@ const getStyle = () => {
           `;
 };
 
-class Tabber extends HTMLElement {
+export default class Tabber extends HTMLElement {
   connectedCallback() {
     this.createShadowRoot();
     const navLinksString = this.getAttribute("navLinks");
@@ -53,12 +53,3 @@ class Tabber extends HTMLElement {
   }
 }
 
-try {
-  customElements.define("tab-link", Tabber);
-} catch (err) {
-  console.error(err);
-  const h3 = document.createElement("h3");
-  h3.innerHTML =
-    "This site uses webcomponents which don't work in all browsers! Try this site in a browser that supports them!";
-  document.body.appendChild(h3);
-}
